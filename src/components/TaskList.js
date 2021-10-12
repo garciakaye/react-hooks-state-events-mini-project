c790acefd271c2715d2c01af02aa25c90df6db0e
kaye garcia <kayemgarcia@gmail.com>
2021-10-12 15:17:48 -0500

    In progress

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import { v4 as uuid } from "uuid";
+import Task from "./Task"
 
-function TaskList() {
+function TaskList({ tasksData }) {
+  const [displayedTasks, setDisplayedTasks] = useState(tasksData)
+
+  function handleDeleteInParent(text){
+    const filteredArr = displayedTasks.filter((task) => task.text !== text)
+    setDisplayedTasks(filteredArr)
+  }
+
+  const taskArr = displayedTasks.map((task) => {
+    return <Task key={uuid()} text={task.text} category={task.category} handleDelete={handleDeleteInParent}/>
+  });
+  
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {taskArr}
     </div>
   );
 }
